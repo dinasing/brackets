97fda720d04070cda2cf7b72c7fba4bde1f76ed2
Dina Singatulina <dina.sing1212@gmail.com>
2019-10-10 13:55:40 +0300

    fix: bracketsConfig iteration

--- src/index.js
+++ src/index.js
@@ -1,13 +1,14 @@
 module.exports = function check(str, bracketsConfig) {
     let array = str.split('');
     let key = 0;
-    bracketsConfig.forEach(brackets => {
-        if (array[0] == brackets[1]) {
-            return false;
-        } 
-    });
+    if (array.length %2 != 0) return false;
+    // bracketsConfig.forEach(brackets => {
+    //     if (array[0] == brackets[1]) {
+    //         return false;
+    //     } 
+    // });
     for(let i = 1; i < array.length; i++) {
-        for(let j = 1; j < bracketsConfig.length; j++) {
+        for(let j = 0; j < bracketsConfig.length; j++) {
             if (array[i] == bracketsConfig[j][1] && array[i-1] == bracketsConfig[j][0]) {
                 key++;
                 break;
