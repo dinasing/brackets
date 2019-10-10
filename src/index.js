4e9b968620c8db736ac4d3069905b42ff72956ad
Dina Singatulina <dina.sing1212@gmail.com>
2019-10-10 13:37:16 +0300

    12 tests passing

--- src/index.js
+++ src/index.js
@@ -1,3 +1,25 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
+    let array = str.split('');
+    let key = 0;
+    bracketsConfig.forEach(brackets => {
+        if (array[0] == brackets[1]) {
+            return false;
+        } 
+    });
+    for(let i = 1; i < array.length; i++) {
+        for(let j = 1; j < bracketsConfig.length; j++) {
+            if (array[i] == bracketsConfig[j][1] && array[i-1] == bracketsConfig[j][0]) {
+                key++;
+                break;
+            }
+            else if (array[i] == bracketsConfig[j][1] && array[i-1-key*2] == bracketsConfig[j][0]) {
+                key++;
+                break;
+            }
+            else if (array[i] == bracketsConfig[j][1] && array[i-1] != bracketsConfig[j][0]) {return false;}
+            if(array[i] == bracketsConfig[j][0]) {break;}
+        }
+
+    }
+    return true;
 }
